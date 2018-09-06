9e22f78a7a2457b21b116aadd9940f0748ffb1a4
Jon Börjesson <jon@spuun.se>
2018-09-06 11:27:27 +0200

    Support for building with mosquitto 1.5

--- auth-plugin.c
+++ auth-plugin.c
@@ -5,13 +5,19 @@
 
 #include <mosquitto.h>
 #include <mosquitto_plugin.h>
+#if MOSQ_AUTH_PLUGIN_VERSION == 3
+#include <mosquitto_broker.h>
+#endif
 #include "go-auth.h"
 
 int mosquitto_auth_plugin_version(void) {
   return MOSQ_AUTH_PLUGIN_VERSION;
 }
-
+#if MOSQ_AUTH_PLUGIN_VERSION == 3
+int mosquitto_auth_plugin_init(void **user_data, struct mosquitto_opt *auth_opts, int auth_opt_count) {
+#else
 int mosquitto_auth_plugin_init(void **user_data, struct mosquitto_auth_opt *auth_opts, int auth_opt_count) {
+#endif
   /*
     Pass auth_opts hash as keys and values char* arrays to Go in order to initialize them there.
   */
@@ -21,7 +27,12 @@ int mosquitto_auth_plugin_init(void **user_data, struct mosquitto_auth_opt *auth
   GoString keys[auth_opt_count];
   GoString values[auth_opt_count];
   int i;
-  struct mosquitto_auth_opt *o;
+
+#if MOSQ_AUTH_PLUGIN_VERSION == 3
+  struct mosquitto_opt *o;
+#else
+	struct mosquitto_auth_opt *o;
+#endif
   for (i = 0, o = auth_opts; i < auth_opt_count; i++, o++) {
     GoString opt_key = {o->key, strlen(o->key)};
     GoString opt_value = {o->value, strlen(o->value)};
@@ -36,20 +47,36 @@ int mosquitto_auth_plugin_init(void **user_data, struct mosquitto_auth_opt *auth
   return MOSQ_ERR_SUCCESS;
 }
 
+#if MOSQ_AUTH_PLUGIN_VERSION == 3
+int mosquitto_auth_plugin_cleanup(void *user_data, struct mosquitto_opt *auth_opts, int auth_opt_count) {
+#else
 int mosquitto_auth_plugin_cleanup(void *user_data, struct mosquitto_auth_opt *auth_opts, int auth_opt_count) {
+#endif
   AuthPluginCleanup();
   return MOSQ_ERR_SUCCESS;
 }
 
+#if MOSQ_AUTH_PLUGIN_VERSION == 3
+int mosquitto_auth_security_init(void *user_data, struct mosquitto_opt *auth_opts, int auth_opt_count, bool reload) {
+#else
 int mosquitto_auth_security_init(void *user_data, struct mosquitto_auth_opt *auth_opts, int auth_opt_count, bool reload) {
+#endif
   return MOSQ_ERR_SUCCESS;
 }
 
+#if MOSQ_AUTH_PLUGIN_VERSION == 3
+int mosquitto_auth_security_cleanup(void *user_data, struct mosquitto_opt *auth_opts, int auth_opt_count, bool reload) {
+#else
 int mosquitto_auth_security_cleanup(void *user_data, struct mosquitto_auth_opt *auth_opts, int auth_opt_count, bool reload) {
+#endif
   return MOSQ_ERR_SUCCESS;
 }
 
+#if MOSQ_AUTH_PLUGIN_VERSION == 3
+int mosquitto_auth_unpwd_check(void *user_data, const struct mosquitto *client, const char *username, const char *password) {
+#else
 int mosquitto_auth_unpwd_check(void *user_data, const char *username, const char *password) {
+#endif
   
   if (username == NULL || password == NULL) {
     printf("error: received null username or password for unpwd check\n");
@@ -66,13 +93,19 @@ int mosquitto_auth_unpwd_check(void *user_data, const char *username, const char
 
   return MOSQ_ERR_AUTH;
 }
-
+#if MOSQ_AUTH_PLUGIN_VERSION == 3
+int mosquitto_auth_acl_check(void *user_data, int access, const struct mosquitto *client, const struct mosquitto_acl_msg *msg) {
+#else
 int mosquitto_auth_acl_check(void *user_data, const char *clientid, const char *username, const char *topic, int access) {
-
-  //Try to print everything that gets here so that we can debug issues.
-  printf("\n\n");
-  printf("auth-plugin.c: starting acl check at auth-plugin.c\n");
-
+#endif
+	//Try to print everything that gets here so that we can debug issues.
+	printf("\n\n");
+	printf("auth-plugin.c: starting acl check at auth-plugin.c\n");
+#if MOSQ_AUTH_PLUGIN_VERSION == 3
+	const char* clientid = mosquitto_client_id(client);
+	const char* username = mosquitto_client_username(client);
+	const char* topic = msg->topic;
+#endif
   if(clientid == NULL) {
     printf("error: clientid is null\n");
   } else {
@@ -118,7 +151,10 @@ int mosquitto_auth_acl_check(void *user_data, const char *clientid, const char *
 
   return MOSQ_ERR_ACL_DENIED;
 }
-
+#if MOSQ_AUTH_PLUGIN_VERSION == 3
+int mosquitto_auth_psk_key_get(void *user_data, const struct mosquitto *client, const char *hint, const char *identity, char *key, int max_key_len) {
+#else
 int mosquitto_auth_psk_key_get(void *user_data, const char *hint, const char *identity, char *key, int max_key_len) {
+#endif
   return MOSQ_ERR_AUTH;
 }
