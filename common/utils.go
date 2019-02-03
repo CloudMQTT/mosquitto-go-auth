46d9bac68e11a57a0096764ed31ef43d0a69f8a2
Carl Hörberg <carl.hoerberg@gmail.com>
2019-02-03 22:56:44 +0100

    use bcrypt instead of PBKDF2

--- common/utils.go
+++ common/utils.go
@@ -13,7 +13,7 @@ import (
 	"time"
 
 	"github.com/pkg/errors"
-	"golang.org/x/crypto/pbkdf2"
+    "golang.org/x/crypto/bcrypt" 
 
 	"github.com/jmoiron/sqlx"
 )
@@ -70,69 +70,12 @@ func match(route []string, topic []string) bool {
 	return false
 }
 
-/*
-* PBKDF2 passwords usage taken from github.com/brocaar/lora-app-server, comments included.
- */
-
-// Generate the hash of a password for storage in the database.
-// NOTE: We store the details of the hashing algorithm with the hash itself,
-// making it easy to recreate the hash for password checking, even if we change
-// the default criteria here.
-// Taken from brocaar's lora-app-server: https://github.com/brocaar/lora-app-server
-func Hash(password string, saltSize int, iterations int, algorithm string) (string, error) {
-	// Generate a random salt value, 128 bits.
-	salt := make([]byte, saltSize)
-	_, err := rand.Read(salt)
-	if err != nil {
-		return "", errors.Wrap(err, "read random bytes error")
-	}
-
-	return hashWithSalt(password, salt, iterations, algorithm), nil
+func Hash(password string, cost int) (string, error) {
+    bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+    return string(bytes), err
 }
 
-// Taken from brocaar's lora-app-server: https://github.com/brocaar/lora-app-server
-func hashWithSalt(password string, salt []byte, iterations int, algorithm string) string {
-	// Generate the hash.  This should be a little painful, adjust ITERATIONS
-	// if it needs performance tweeking.  Greatly depends on the hardware.
-	// NOTE: We store these details with the returned hash, so changes will not
-	// affect our ability to do password compares.
-	shaSize := sha512.Size
-	shaHash := sha512.New
-	if algorithm == "sha256" {
-		shaSize = sha256.Size
-		shaHash = sha256.New
-	}
-	hash := pbkdf2.Key([]byte(password), salt, iterations, shaSize, shaHash)
-
-	// Build up the parameters and hash into a single string so we can compare
-	// other string to the same hash.  Note that the hash algorithm is hard-
-	// coded here, as it is above.  Introducing alternate encodings must support
-	// old encodings as well, and build this string appropriately.
-	var buffer bytes.Buffer
-
-	buffer.WriteString("PBKDF2$")
-	buffer.WriteString(fmt.Sprintf("%s$", algorithm))
-	buffer.WriteString(strconv.Itoa(iterations))
-	buffer.WriteString("$")
-	buffer.WriteString(base64.StdEncoding.EncodeToString(salt))
-	buffer.WriteString("$")
-	buffer.WriteString(base64.StdEncoding.EncodeToString(hash))
-
-	return buffer.String()
-}
-
-// HashCompare verifies that passed password hashes to the same value as the
-// passed passwordHash.
-// Taken from brocaar's lora-app-server: https://github.com/brocaar/lora-app-server
 func HashCompare(password string, passwordHash string) bool {
-	// SPlit the hash string into its parts.
-	hashSplit := strings.Split(passwordHash, "$")
-
-	// Get the iterations and the salt and use them to encode the password
-	// being compared.cre
-	iterations, _ := strconv.Atoi(hashSplit[2])
-	salt, _ := base64.StdEncoding.DecodeString(hashSplit[3])
-	algorithm := hashSplit[1]
-	newHash := hashWithSalt(password, salt, iterations, algorithm)
-	return newHash == passwordHash
+    err := bcrypt.CompareHashAndPassword([]byte(passwordHash), []byte(password))
+    return err == nil
 }
--- pw-gen/pw.go
+++ pw-gen/pw.go
@@ -3,22 +3,17 @@ package main
 import (
 	"flag"
 	"fmt"
-
-	"github.com/iegomez/mosquitto-go-auth/common"
+    "github.com/cloudmqtt/mosquitto-go-auth/common"
 )
-
-// saltSize defines the salt size
-const saltSize = 16
 
 func main() {
 
-	var algorithm = flag.String("a", "sha512", "algorithm (sha256 or default: sha512)")
-	var HashIterations = flag.Int("i", 100000, "hash iterations (default: 100000)")
+	var cost = flag.Int("c", 10, "bcrypt cost (default 10)")
 	var password = flag.String("p", "", "password")
 
 	flag.Parse()
 
-	pwHash, err := common.Hash(*password, saltSize, *HashIterations, *algorithm)
+	pwHash, err := common.Hash(*password, *cost)
 	if err != nil {
 		fmt.Errorf("error: %s\n", err)
 	} else {
